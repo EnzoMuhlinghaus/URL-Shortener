70782cc2946af049594452373a7f291616424123
Enzo Muhlinghaus <enzomuhlinghaus@gmail.com>
2017-09-28 10:36:38 +0200

    Set up redirection

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -3,45 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Link;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class LinksController extends Controller
 {
+
+  public function redirect($id){
+    $link = Link::findOrFail($id);
+
+    return new RedirectResponse($link->url, 301);
+  }
     /**
      * Display a listing of the resource.
      * GET /links
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-      $links = Link::all();
-      return response($links, 200);
-    }
-    /**
-     * Store a newly created resource in storage.
-     * POST /links
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $link = Link::create($request->all());
+  public function index()
+  {
+    $links = Link::all();
+    return response($links, 200);
+  }
 
-      return response($link->id, 201);
-    }
+  /**
+   * Store a newly created resource in storage.
+   * POST /links
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $link = Link::create($request->all());
 
-    /**
-     * Display the specified resource.
-     * GET /links/{id}
-     *
-     * @param  \App\Link  $link
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Link $link)
-    {
-      $link = Link::findOrFail($link);
-      return response($link, 200);
-    }
+    return response($link->id, 201);
+  }
+
+  /**
+   * Display the specified resource.
+   * GET /links/{id}
+   *
+   * @param  \App\Link $link
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Link $link)
+  {
+    $linkShown = Link::findOrFail($link);
+    return response($linkShown, 200);
+  }
 }
